61898e27f280951408bff26b5724430bc24616c3
Arthurwang8 <148507250+Arthurwang8@users.noreply.github.com>
2023-11-01 12:41:45 -0700

    Update starT.cpp

--- starT.cpp
+++ starT.cpp
@@ -14,7 +14,31 @@ void runTests(void);
 string starT(int width, int height)
 {
   string result = "";
-  result = "stub"; // TODO: remove this line, replace with correct code
+  // check if parameters are valid
+  if (height <= 1||width%2 == 0){
+    return result; // return without printing anything
+    }
+  // add the first height-1 rows that are a single star
+  // followed by width-1 spaces, then a \n
+  for (int co = 0; co < width; co++)
+  {
+  result += "*";
+    
+  }
+  result += "\n";
+
+  // add the final row of width stars
+  for (int row = 1; row < height; row++)
+  {
+    for (int co = 1; co < width/2; co++)
+  {
+  result += " ";
+    
+  }
+    result += "*";
+    result += "\n";
+  }
+  
   return result;
 }
 
@@ -78,17 +102,23 @@ void assertEquals(string expected, string actual, string message = "")
 int main(int argc, char *argv[])
 {
 
-  // TODO: Add check for parameters
-  // and code to print usage message
+ if (argc != 3)
+  {
+    cerr << "Usage: " << argv[0] << " width height" << endl;
+    exit(1);
+  }
 
-  // TODO: Add code to get width and height from command line args
-  // code that checks if they are both -1; if so, call runTests()
-  // then exit.
+  int width = stoi(argv[1]);
+  int height = stoi(argv[2]);
+  // If the program is executed with parameters -1 -1 unit test
+  // the starL() function using our automated test framework
+  if (width == -1 && height == -1)
+  {
+    runTests();
+    exit(1);
+  }
 
-  runTests();
-
-  // TODO: Add code that calls the starT function and prints
-  // the result on cout (without an extra newline)
-
+  cout << starT(width, height);
   return 0;
+
 }
